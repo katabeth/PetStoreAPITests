54e1330b5f4f7525b2b3150707fc3de89222cf0f
Liam <33605657+l-hepper@users.noreply.github.com>
2024-09-04 15:54:38 +0100

    adds basic test for getting pet by ID

--- PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/PetStoreApiTestingAppApplicationTests.java
+++ PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/PetStoreApiTestingAppApplicationTests.java
@@ -1,9 +1,7 @@
 package org.example.petstoreapitestingapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PetStoreApiTestingAppApplicationTests {
 
     @Test
