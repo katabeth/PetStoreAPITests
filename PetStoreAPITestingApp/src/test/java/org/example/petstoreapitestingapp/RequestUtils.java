1a8f3e8e6408c3b5712eb1cd4a0988fd845b1921
DevColyer <dcolyer@spartaglobal.com>
2024-09-04 15:28:06 +0100

    Static request utils class

---
+++ PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/RequestUtils.java
@@ -0,0 +1,37 @@
+package org.example.petstoreapitestingapp;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.Map;
+
+public class RequestUtils {
+    public static RequestSpecification getRequestSpec(String baseUri, String path, Map<String, String> headers, Map<String, String> pathParams) {
+        return new RequestSpecBuilder()
+                .setBaseUri(baseUri)
+                .setBasePath(path)
+                .addHeaders(headers)
+                .addPathParams(pathParams)
+                .build();
+    }
+
+    public static RequestSpecification postRequestSpec(String baseUri, String path, Map<String, String> headers, Map<String, String> pathParams, Map<String, ?> body) {
+        return new RequestSpecBuilder()
+                .setBaseUri(baseUri)
+                .setBasePath(path)
+                .addHeaders(headers)
+                .addPathParams(pathParams)
+                .setContentType(ContentType.JSON)
+                .setBody(body)
+                .build();
+    }
+
+    public static RequestSpecification deleteRequestSpec(String baseUri, String path, Map<String, String> headers, Map<String, String> pathParams) {
+        return getRequestSpec(baseUri, path, headers, pathParams);
+    }
+
+    public static RequestSpecification putRequestSpec(String baseUri, String path, Map<String, String> headers, Map<String, String> pathParams, Map<String,?> body) {
+        return postRequestSpec(baseUri, path, headers, pathParams, body);
+    }
+}
