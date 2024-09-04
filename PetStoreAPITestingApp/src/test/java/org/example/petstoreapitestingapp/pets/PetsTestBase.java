754fdda0ae2ddac1b02b0e380c2f6d0b65d762b6
DevColyer <dcolyer@spartaglobal.com>
2024-09-04 15:22:46 +0100

    Initial base classes for extensions

---
+++ PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/pets/PetsTestBase.java
@@ -0,0 +1,9 @@
+package org.example.petstoreapitestingapp.pets;
+
+import org.example.petstoreapitestingapp.TestBase;
+
+public abstract class PetsTestBase extends TestBase {
+    protected Integer petId;
+    protected Pet petResponse;
+    protected Pet[] pets;
+}
