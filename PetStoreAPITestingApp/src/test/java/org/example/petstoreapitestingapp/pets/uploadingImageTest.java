00cc680801d2a7191cc093c5a0369b9eaecc6b0c
Katherine <katabeth.ch@gmail.com>
2024-09-05 15:35:55 +0100

    I cannot do uploading an image

--- PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/pets/uploadingImageTest.java
+++ PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/pets/uploadingImageTest.java
@@ -8,6 +8,7 @@ import org.example.petstoreapitestingapp.pojo.Pet;
 import org.hamcrest.MatcherAssert;
 import org.junit.jupiter.api.*;
 
+import java.io.File;
 import java.util.Map;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -16,6 +17,8 @@ public class uploadingImageTest extends PetsTestBase{
     private static Response response;
     private static final String PATH = "/pet/{petId}/uploadImage";
     private static Pet returnedPet;
+    File imageFile = new File("path/to/your/image.jpg");
+    File file = new File("src/test/resources/cult_fanatic.png");
 //    curl -X 'POST' \
 //            'https://petstore3.swagger.io/api/v3/pet/55/uploadImage' \
 //            -H 'accept: application/json' \
@@ -23,29 +26,29 @@ public class uploadingImageTest extends PetsTestBase{
 //            --data-binary '@380px-Disguise_Self_Masc_Human.webp.png'
     @BeforeAll
     public static void setUp() {
+
     }
     @AfterAll
     public static void tearDown() {
     }
 
+
+    // Perform the POST request with the image as multipart form-data
+     // Log the response
     @Test
     @Order(1)
-    @DisplayName("Add an image to an exisiting pet gives 200 repsosne")
-    public void addAnImageToAnExisitingPetGives200Reposne() {
+    @DisplayName("Add an image to an existing pet gives 200 response")
+    public void addAnImageToAnExistingPetGives200Response() {
         response = RestAssured
                 .given()
                 .baseUri(BASE_URI)
                 .basePath(PATH)
                 .pathParam("petId", 55)
-                .relaxedHTTPSValidation()
-                .headers(Map.of(
-                        "accept", "application/json",
-                        "Content-Disposition: attachment; filename", "https://www.dndbeyond.com/avatars/thumbnails/43939/81/1000/1000/638607414147450285.png"
-                ))
-                .body("https://www.dndbeyond.com/avatars/thumbnails/43939/81/1000/1000/638607414147450285.png")
-                .log().all()
+                .multiPart("@file", file) // Attach the image as a multiPart field
+                .multiPart("additionalMetadata", "test metadata") // Add metadata as multipart field
+                .contentType(ContentType.MULTIPART) // Set content type as multipart/form-data
                 .when()
-                .post()
+                .post() // API endpoint for uploading the image
                 .thenReturn();
         response.prettyPrint();
         //returnedPet = response.as(Pet.class);
