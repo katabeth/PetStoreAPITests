380f9441515a30c28e1bde964cc0c547573a2279
Katherine <katabeth.ch@gmail.com>
2024-09-05 10:12:38 +0100

    Add and update tests run, are ordered and are checked against the swagger ui

--- PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/pets/addAndUpdateNewPets.java
+++ PetStoreAPITestingApp/src/test/java/org/example/petstoreapitestingapp/pets/addAndUpdateNewPets.java
@@ -7,21 +7,38 @@ import org.example.petstoreapitestingapp.pojo.Category;
 import org.example.petstoreapitestingapp.pojo.Pet;
 import org.example.petstoreapitestingapp.pojo.TagsItem;
 import org.hamcrest.MatcherAssert;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.util.List;
 import java.util.Map;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class addAndUpdateNewPets extends PetsTestBase{
 
     private static Response response;
+    private static Pet newPet;
+    private static Pet updatePet;
+    private static TagsItem tagsItem;
 
     @BeforeAll
     public static void setUp() {
-
+        tagsItem = new TagsItem(0,"String");
+        newPet = new Pet(
+                55,
+                "Lion",
+                new Category(2, "Cats"),
+                List.of("string"),
+                List.of(tagsItem),
+                "available"
+        );
+        updatePet = new Pet(
+                55,
+                "Potatoes",
+                new Category(2, "Cats"),
+                List.of("string"),
+                List.of(tagsItem),
+                "available"
+        );
     }
     @AfterAll
     public static void tearDown() {
@@ -29,29 +46,47 @@ public class addAndUpdateNewPets extends PetsTestBase{
     }
 
     @Test
+    @Order(1)
     @DisplayName("Add new pet to the store")
     public void addNewPet() {
-        TagsItem tagsItem = new TagsItem(0,"String");
-        Pet newPet = new Pet(
-                10,
-                "Lion",
-                new Category(2, "Cats"),
-                List.of("string"),
-                List.of(tagsItem),
-                "available"
-        );
-
         response = RestAssured
                 .given()
                 .baseUri(PetsTestBase.BASE_URI)
                 .basePath("/pet")
                 .contentType(ContentType.JSON)
                 .body(newPet)
-                .log().all()
                 .when()
                 .post()
                 .thenReturn();
+
         MatcherAssert.assertThat(response.getStatusCode(), org.hamcrest.Matchers.is(200));
     }
+    @Test
+    @Order(2)
+    @DisplayName("Check added pet gives correct name")
+    public void checkAddedPetGivesCorrectName() {
+        MatcherAssert.assertThat(response.getBody().jsonPath().getString("name"), org.hamcrest.Matchers.is("Lion"));
+    }
 
+    @Test
+    @Order(3)
+    @DisplayName("Update pet in the store")
+    public void updatePet() {
+        response = RestAssured
+                .given()
+                .baseUri(PetsTestBase.BASE_URI)
+                .basePath("/pet")
+                .contentType(ContentType.JSON)
+                .body(updatePet)
+                .when()
+                .put()
+                .thenReturn();
+        MatcherAssert.assertThat(response.getStatusCode(), org.hamcrest.Matchers.is(200));
+    }
+    @Test
+    @Order(4)
+    @DisplayName("Check update gives correct name")
+    public void checkUpdateGivesCorrectName() {
+        MatcherAssert.assertThat(response.getBody().jsonPath().getString("name"), org.hamcrest.Matchers.is("Potatoes"));
+        }
 }
